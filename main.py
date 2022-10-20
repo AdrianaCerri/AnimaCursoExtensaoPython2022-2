faab9585b203c541a8c9c392ff4193135e72f857
adrianacerri <111378679+AdrianaCerri@users.noreply.github.com>
2022-10-20 00:53:42 +0000

    criei a variável nome e exibi na tela

--- main.py
+++ main.py
@@ -3,6 +3,14 @@
 # print() = comando de saida
 print("Alo mundo!")
 
+# Quando quiser guardar uma string!
+nome = "Adriana"
+#Quando quiser guardar um número inteiro
+idade = 20
+
+#Exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
 
 
 '''
