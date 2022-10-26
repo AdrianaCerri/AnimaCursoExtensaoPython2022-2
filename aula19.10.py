402b4527e3da3fc76e99116e48b34223c32b1e2e
adrianacerri <111378679+AdrianaCerri@users.noreply.github.com>
2022-10-26 23:13:40 +0000

    Comando de entrada e input

--- aula19.10.py
+++ aula19.10.py
@@ -13,30 +13,18 @@ print(nome)
 
 #Quando quiser exibir a frase "Minha idade é" completando com o conteúdo de variável idade
 #print ("Meu nome é "+nome)
-print("Minha idade é " + str(idade) + " anos")
+print("Minha idade é "+str(idade)+" anos")
 print(f"Minha idade é {idade} anos")
 print("Minha idade é {} anos".format(idade))
 
 #Quando quiser exibir "Meu nome é ... e tenho ... anos" trocando pelas variáveis nome e idade
 print("Meu nome é {} e tenho {} anos".format(nome, idade))
+
+
+
+
+
 '''
 Comentario em bloco
 
 '''
-
-#comando input(): quero permitir que o usuário digite algo
-nome = input("Digite seu nome: ")
-
-#pede a idade para o usuário "Qual sua idade?"
-idade = int(input("Digite sua idade: "))
-
-#comando de saída..exibir na tela
-print(f"Boa noite, seu nome é {nome}")
-
-#comando de saída..exiba "sua idade é"
-print(f"Sua idade é {idade}")
-
-#e se eu quisesse mostrar o dobro da idade?
-dobro = idade * 2 
-print("O dobro da idade informada é {}".format(dobro))
-
